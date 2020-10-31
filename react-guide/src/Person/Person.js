2a4c293f859c63211a2fd7c55d6f61273354a81c
Alvaro <alvar01omer@gmail.com>
2020-10-30 21:59:38 -0300

    children

--- react-guide/src/Person/Person.js
+++ react-guide/src/Person/Person.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const person = (props) => {
-return <p> I'm {props.name}, and I'm {props.age}! Random Number: {Math.floor(Math.random() *30)}</p>
+    return (
+        <div>
+            <p> I'm {props.name}, and I'm {props.age}! Random Number: {Math.floor(Math.random() *30)} </p>
+            {props.children}
+        </div>
+    )
 }
+
 
 export default person;
