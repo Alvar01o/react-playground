04ec329d141f9f61b499c69a36f71d2d49a14fb7
Alvaro <alvar01omer@gmail.com>
2020-10-30 21:51:49 -0300

    dinamyc content

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -8,6 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Person />
+        <Person />
+        <Person />
         <a
           className="App-link"
           href="https://reactjs.org"
