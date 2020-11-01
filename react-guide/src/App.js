8b29f4a8527973c38607471375f91866782b1fbe
Alvaro <alvar01omer@gmail.com>
2020-11-01 18:07:51 -0300

    useState hook

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -1,32 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Person  from './Person/Person.js'
 
 
 
 function App() {
 
-  const persons =  [
+const [ personsState , setPersonsState ] = useState({
+  persons: [
     { name: 'Max', age: 28 },
     { name: 'Alvaro', age: 29 },
     { name: 'Stephanie', age: 26 }
   ]
+})
+
+  const switchNameHandler = () => {
+    console.log("Click")
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Person name={persons[0].name} age={persons[0].age} />
-        <Person name={persons[1].name} age={persons[1].age} >My hobbies : Programming </Person>
-        <Person name={persons[2].name} age={persons[2].age} />
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age} >My hobbies : Programming </Person>
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+        <button onClick={switchNameHandler}>Switch Name</button>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+
         </a>
       </header>
     </div>
