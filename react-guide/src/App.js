c46c9c90af15739e3ff8d79fc8054c5b140e3454
Alvaro <alvar01omer@gmail.com>
2020-11-13 17:29:31 -0300

    class based app.js

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -1,73 +1,83 @@
 
 import './App.css';
-import React, { useState } from 'react';
+import React, { Component} from 'react';
 import Persons  from './components/Persons/Persons.js'
 import Cockpit  from './components/Cockpit/Cockpit.js'
 
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log("[App.js] contructor")
+  }
 
-const [ personsState , setPersonsState ] = useState({
-  persons: [
-    {id:'1', name: 'Max', age: 28 },
-    {id:'2', name: 'Alvaro', age: 29 },
-    {id:'3', name: 'Stephanie', age: 26 }
-  ],
-  showPersons:false
-})
+  state = {
+    persons: [
+      {id:'1', name: 'Max', age: 28 },
+      {id:'2', name: 'Alvaro', age: 29 },
+      {id:'3', name: 'Stephanie', age: 26 }
+    ],
+    showPersons:false
+  }
 
 
-
-  const nameChangeHandler = (event , id) => {
-    const personIndex = personsState.persons.findIndex(p=>{
+  nameChangeHandler = (event , id) => {
+    const personIndex = this.state.persons.findIndex(p=>{
       return p.id === id 
     })
     const person = {
-      ...personsState.persons[personIndex]
+      ...this.state.persons[personIndex]
     };
 
     person.name = event.target.value;
 
-    const persons = [...personsState.persons];
+    const persons = [...this.state.persons];
     persons[personIndex] = person;
-
-    setPersonsState({
-      persons: persons,
-      showPersons: personsState.showPersons
+    this.setState({
+      persons: persons
     })
   }
 
-  const togglePersonsHandler = () => {
-    const doesShow = personsState.showPersons;
-    setPersonsState({persons:personsState.persons,showPersons: !doesShow})
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({
+      showPersons :!doesShow
+    })
   }
 
-  const deletePersonHandler = (index) => {  
-//    const persons = personsState.persons.slice();
-    const persons = [...personsState.persons]
+  deletePersonHandler = (index) => {  
+//    const persons = this.state.persons.slice();
+    const persons = [...this.state.persons]
     persons.splice(index , 1 )
-    setPersonsState({persons:persons, showPersons: personsState.showPersons})
+    this.setState({
+      persons :persons
+    })
   }
-  let persons = null;
 
-  if(personsState.showPersons) {
-    persons = (
-      <div>
-        <Persons click={deletePersonHandler}  changed={nameChangeHandler} persons={personsState.persons}> </Persons>
+
+
+  render(){
+
+    let persons = null;
+
+    if(this.state.showPersons) {
+      persons = (
+        <div>
+          <Persons click={this.deletePersonHandler}  changed={this.nameChangeHandler} persons={this.state.persons}> </Persons>
+        </div>
+      )
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Cockpit click={this.togglePersonsHandler} persons={this.state.persons}></Cockpit>
+          {persons}
+        </header>
       </div>
-    )
+    );
   }
 
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Cockpit click={togglePersonsHandler} persons={personsState.persons}></Cockpit>
-        {persons}
-      </header>
-    </div>
-  );
-//return React.createElement('div',{className:"App"}, React.createElement('h1',null,'Learn React'))
+  
 }
-
 export default App;
