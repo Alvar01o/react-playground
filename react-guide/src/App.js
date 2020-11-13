25d9c5a37761d13184f730aedd501a4c150d113a
Alvaro <alvar01omer@gmail.com>
2020-11-13 20:27:41 -0300

    hooks

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -20,7 +20,27 @@ class App extends Component {
     showPersons:false
   }
 
+//  static getDerivedStateFromProps(props, state){
+//    console.log("[App.js] getDerivedStateFromProps", props);
+//    return state;
+//  }
 
+  componentWillMount(){
+    console.log("[App.js] componentWillMount");
+  }
+
+  componentDidMount(){
+    console.log("[App.js] componentDidMount")
+  }
+
+  shouldComponentUpdate(nextProps, nextState){
+    console.log("[App.js] shouldComponentUpdate")      
+    return true;
+  }
+
+  componentDidUpdate(){
+    console.log("[App.js] componentDidUpdate") 
+  }
   nameChangeHandler = (event , id) => {
     const personIndex = this.state.persons.findIndex(p=>{
       return p.id === id 
